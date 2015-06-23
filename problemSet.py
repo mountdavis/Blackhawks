7ad426ceb729a0d2edb7f5e54f8f6f842080fd83
Mount Davis <mountdavis@users.noreply.github.com>
2015-06-23 17:08:42 +0800

    Create problemSet.py
    this is the file that want to push in the repository... but it is being cut&paste from a text editor. not select from a pull-down menu

---
+++ problemSet.py
@@ -0,0 +1,31 @@
+import os
+import string
+import time
+
+prompt = ">"
+prompt2 = ">>"
+
+def isdatevalid (yyyy,mm,dd):
+    correctDate = None
+    try:
+        newDate = datetime.datetime(yyyy,mm,dd)
+        correctDate = True
+    except ValueError:
+        correctDate = False
+    print(str(correctDate))
+
+print "enter your date of birth and your last name"
+print "Please enter your date of birth:"
+print "year of birth: (yyyy)"
+dobyyyy = raw_input(prompt)
+print "month of birth: (mm)"
+dobmm = raw_input(prompt)
+print "days of birth: (dd)"
+dobdd = raw_input(prompt)
+#check the date is valid or not
+isdatevalid(dobyyyy, dobmm, dobdd)
+print "Please enter your last name"
+lastname = raw_input(prompt2)
+
+print "your date of birth and last name as follow"
+print lastname +" "+ dob
